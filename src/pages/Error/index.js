e2123e86f6dd596a0155de7ee12f5df3f23ceab7
moeIsAwesome <musavimoe@gmail.com>
2022-02-18 20:10:11 +0330

    Back button to error page

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,18 @@
 import styles from './Error.module.css';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Error() {
+  const Navigate = useNavigate();
   return (
     <div className={styles.App}>
       <h1>ERRPR!</h1>
+      <Button
+        text="Back"
+        functionalityOnClick={() => {
+          Navigate('/wednesdays');
+        }}
+      />
     </div>
   );
 }
